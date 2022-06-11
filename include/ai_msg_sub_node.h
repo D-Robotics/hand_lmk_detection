044f9c0ba2180a460328d6f37fa9aad7768524b6
kao.zhu <kao.zhu@horizon.ai>
2022-06-11 22:57:12 +0800

    fix(hand_lmk_det): [cr_id_skip] Fix ai msg cache issue
    Test Plan: verified
    
    Reviewers: kairui.wang
    
    Reviewed By: kairui.wang
    
    Differential Revision: https://cr.hobot.cc/D184997

--- include/ai_msg_sub_node.h
+++ include/ai_msg_sub_node.h
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <memory>
-#include <string>
-#include <vector>
 #include <map>
-#include <utility>
+#include <memory>
 #include <queue>
+#include <string>
 #include <unordered_map>
+#include <utility>
+#include <vector>
 
 #include "ai_msgs/msg/capture_targets.hpp"
 #include "ai_msgs/msg/perception_targets.hpp"
@@ -34,10 +34,19 @@ using rclcpp::NodeOptions;
 using feed_predict_type =
     std::shared_ptr<std::pair<ai_msgs::msg::PerceptionTargets::ConstSharedPtr,
                               sensor_msgs::msg::Image::ConstSharedPtr>>;
+
+struct compare_msg {
+  bool operator()(const std_msgs::msg::Header::_stamp_type m1,
+                  const std_msgs::msg::Header::_stamp_type m2) {
+    return ((m1.sec > m2.sec) ||
+            ((m1.sec == m2.sec) && (m1.nanosec > m2.nanosec)));
+  }
+};
+
 class HandLmkFeedCache {
  public:
-  explicit HandLmkFeedCache(int cache_lint_len = 10) :
-  cache_lint_len_(cache_lint_len) {}
+  explicit HandLmkFeedCache(int cache_lint_len = 20)
+      : cache_limt_len_(cache_lint_len) {}
   ~HandLmkFeedCache() {}
 
   int Feed(const ai_msgs::msg::PerceptionTargets::ConstSharedPtr& msg) {
@@ -50,20 +59,54 @@ class HandLmkFeedCache {
     ai_msg->set__perfs(msg->perfs);
     std::string ts = std::to_string(msg->header.stamp.sec) + "." +
                      std::to_string(msg->header.stamp.nanosec);
-    RCLCPP_DEBUG(rclcpp::get_logger("example"), "Feed ts %s", ts.c_str());
+    RCLCPP_DEBUG(
+        rclcpp::get_logger("hand lmk ai msg sub"), "Feed ts %s", ts.c_str());
 
     std::unique_lock<std::mutex> lg(cache_mtx_);
     // 判断是否超过最大长度
-    if (recved_input_cache_.size() > cache_lint_len_) {
-      recved_input_cache_.erase(recved_input_cache_.begin());
+    if (recved_aimsg_cache_.size() > cache_limt_len_) {
+      // 删掉最小时间戳
+      std::string top_ts = std::to_string(recved_aimsg_ts_.top().sec) + "." +
+                           std::to_string(recved_aimsg_ts_.top().nanosec);
+      RCLCPP_WARN(rclcpp::get_logger("hand lmk ai msg sub"),
+                  "Ai msg cache len: %d exceeds limit: %d, erase ai ts: %s",
+                  recved_aimsg_cache_.size(),
+                  cache_limt_len_,
+                  top_ts.data());
+      recved_aimsg_cache_.erase(top_ts);
+      recved_aimsg_ts_.pop();
     }
+
+    if (recved_aimsg_ts_.size() > cache_limt_len_) {
+      std::string top_ts = std::to_string(recved_aimsg_ts_.top().sec) + "." +
+                           std::to_string(recved_aimsg_ts_.top().nanosec);
+      RCLCPP_WARN(rclcpp::get_logger("hand lmk ai msg sub"),
+                  "Ts cache len: %d exceeds limit: %d, erase ts: %s",
+                  recved_aimsg_ts_.size(),
+                  cache_limt_len_,
+                  top_ts.data());
+      recved_aimsg_cache_.erase(top_ts);
+      recved_aimsg_ts_.pop();
+    }
+
     recved_aimsg_cache_[ts] = std::move(ai_msg);
+    recved_aimsg_ts_.push(msg->header.stamp);
+    RCLCPP_DEBUG(rclcpp::get_logger("hand lmk ai msg sub"),
+                 "top ts: %llu %llu, cache size: %d, ts size: %d",
+                 recved_aimsg_ts_.top().sec,
+                 recved_aimsg_ts_.top().nanosec,
+                 recved_aimsg_cache_.size(),
+                 recved_aimsg_ts_.size());
     return 0;
   }
 
-  ai_msgs::msg::PerceptionTargets::UniquePtr Get(const std::string& ts,
-                                                 int time_out_ms = 1000) {
-    RCLCPP_DEBUG(rclcpp::get_logger("example"), "Get ts %s", ts.c_str());
+  ai_msgs::msg::PerceptionTargets::UniquePtr Get(
+      const std_msgs::msg::Header::_stamp_type& msg_ts,
+      int time_out_ms = 1000) {
+    std::string ts =
+        std::to_string(msg_ts.sec) + "." + std::to_string(msg_ts.nanosec);
+    RCLCPP_DEBUG(
+        rclcpp::get_logger("hand lmk ai msg sub"), "Get ts %s", ts.c_str());
 
     ai_msgs::msg::PerceptionTargets::UniquePtr feed_predict = nullptr;
     std::unique_lock<std::mutex> lg(cache_mtx_);
@@ -73,18 +116,54 @@ class HandLmkFeedCache {
     if (recved_aimsg_cache_.find(ts) != recved_aimsg_cache_.end()) {
       feed_predict = std::move(recved_aimsg_cache_.at(ts));
       recved_aimsg_cache_.erase(ts);
+
+      RCLCPP_DEBUG(rclcpp::get_logger("hand lmk ai msg sub"),
+                   "find ts %s success, recved_aimsg_ts_ top ts: %llu %llu, "
+                   "cache size: %d, ts size: %d",
+                   ts.c_str(),
+                   recved_aimsg_ts_.top().sec,
+                   recved_aimsg_ts_.top().nanosec,
+                   recved_aimsg_cache_.size(),
+                   recved_aimsg_ts_.size());
+
+      // 清理过期的时间戳缓存
+      while (!recved_aimsg_ts_.empty() &&
+             recved_aimsg_ts_.size() > recved_aimsg_cache_.size()) {
+        if ((recved_aimsg_ts_.top() == msg_ts) ||
+            (recved_aimsg_ts_.top().sec < msg_ts.sec) ||
+            (recved_aimsg_ts_.top().sec == msg_ts.sec &&
+             recved_aimsg_ts_.top().nanosec < msg_ts.nanosec)) {
+          std::string top_ts = std::to_string(recved_aimsg_ts_.top().sec) +
+                               "." +
+                               std::to_string(recved_aimsg_ts_.top().nanosec);
+          recved_aimsg_cache_.erase(top_ts);
+          recved_aimsg_ts_.pop();
+
+          RCLCPP_DEBUG(rclcpp::get_logger("hand lmk ai msg sub"),
+                       "Erase top ts: %s, cache len: %d ts len: %d",
+                       top_ts.data(),
+                       recved_aimsg_cache_.size(),
+                       recved_aimsg_ts_.size());
+        } else {
+          break;
+        }
+      }
     }
 
     return feed_predict;
   }
 
  private:
-  std::queue<feed_predict_type> feed_predict_cache_;
-  // key is ts
-  std::unordered_map<std::string, feed_predict_type> recved_input_cache_;
-  size_t cache_lint_len_ = 10;
+  // 缓存的长度限制
+  size_t cache_limt_len_ = 20;
   std::mutex cache_mtx_;
   std::condition_variable cache_cv_;
+  // 缓存AI结果的时间戳
+  std::priority_queue<std_msgs::msg::Header::_stamp_type,
+                      std::vector<std_msgs::msg::Header::_stamp_type>,
+                      compare_msg>
+      recved_aimsg_ts_;
+  // key is ts
   std::unordered_map<std::string, ai_msgs::msg::PerceptionTargets::UniquePtr>
       recved_aimsg_cache_;
 };
@@ -95,7 +174,7 @@ class AiMsgSubNode : public rclcpp::Node {
                const NodeOptions& options = NodeOptions());
   ~AiMsgSubNode() override;
 
-  int GetTargetRois(const std::string& ts,
+  int GetTargetRois(const std_msgs::msg::Header::_stamp_type& msg_ts,
                     std::shared_ptr<std::vector<hbDNNRoi>>& rois,
                     std::map<size_t, size_t>& valid_roi_idx,
                     ai_msgs::msg::PerceptionTargets::UniquePtr& ai_msg,
@@ -111,7 +190,7 @@ class AiMsgSubNode : public rclcpp::Node {
   std::string ai_msg_sub_topic_name_ = "/hobot_mono2d_body_detection";
   rclcpp::Subscription<ai_msgs::msg::PerceptionTargets>::SharedPtr
       ai_msg_subscription_ = nullptr;
-  void AiImgProcess(const ai_msgs::msg::PerceptionTargets::ConstSharedPtr msg);
+  void AiMsgProcess(const ai_msgs::msg::PerceptionTargets::ConstSharedPtr msg);
 };
 
 #endif  // AI_MSG_SUB_NODE_H_
