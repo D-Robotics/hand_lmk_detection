455abc11fb79ebdc0fb2080edb6a1945a226fb23
kao.zhu <kao.zhu@d-robotics.cc>
2024-11-15 21:01:25 +0800

    feat: Change opencv version from 3.4.5 to 4.x

--- src/hand_lmk_det_node.cpp
+++ src/hand_lmk_det_node.cpp
@@ -35,6 +35,7 @@
 #include "opencv2/core/mat.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/imgproc/types_c.h"
 
 builtin_interfaces::msg::Time ConvertToRosTime(
     const struct timespec& time_spec) {
